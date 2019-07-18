630c70b78f602d14febb4240968a4eeac67cb977
TangGuoer <53026850+TANGGUOER@users.noreply.github.com>
2019-07-18 16:29:23 +0800

    Initial commit

---
+++ src/main/java/com/iweb/springboot/controller/HelloBoot.java
@@ -0,0 +1,26 @@
+package com.iweb.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author azzhu
+ * @create 2019-07-17 09:23:38
+ */
+@RestController
+//@Controller
+//@ResponseBody
+public class HelloBoot {
+
+    @GetMapping("/hello")
+    public Object hello() {
+        List<String> list = new ArrayList<>();
+        list.add("zzzz");
+        list.add("bbbbb");
+        list.add("ccccc");
+        return list;
+    }
+}
